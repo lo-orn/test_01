25f63a772f2bfa09aaf300a5580521925c89f1c7
Lo <lovisaornestal@gmail.com>
2025-10-20 15:07:18 +0200

    test on objects

--- src/main.js
+++ src/main.js
@@ -1,1 +1,42 @@
 import "./style.css";
+class Person {
+  name;
+  age;
+  length;
+  eyecolor;
+
+  constructor(name, age, length, eyecolor) {
+    this.name = name;
+    this.age = age;
+    this.length = length;
+    this.eyecolor = eyecolor;
+  }
+}
+
+const me = new Person("Lo", 32, "170cm", "blue");
+const wife = new Person("Isabella", 32, "163cm", "brown");
+
+const family = [me, wife];
+
+family.forEach((person) => {
+  const familyDiv = document.createElement("div");
+  familyDiv.className = "familyDiv";
+
+  const name = document.createElement("h2");
+  name.innerHTML = person.name;
+
+  const age = document.createElement("p");
+  age.innerHTML = person.age;
+
+  const length = document.createElement("p");
+  length.innerHTML = person.length;
+
+  const eyecolor = document.createElement("p");
+  eyecolor.innerHTML = person.eyecolor;
+
+  familyDiv.appendChild(name);
+  familyDiv.appendChild(age);
+  familyDiv.appendChild(length);
+  familyDiv.appendChild(eyecolor);
+  app.appendChild(familyDiv);
+});
